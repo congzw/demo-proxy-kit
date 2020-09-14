fd0dfdc6078bd61e9fb5a9e585a4620d317a0d8d
congzw <2981810300@qq.com>
2020-09-14 18:29:48 +0800

    init projects

---
+++ readme.md
@@ -0,0 +1,8 @@
+# a demo project for ProxyKit to a demo sites
+
+- DemoSites is a proxy site: "http://localhost:5066"
+- DemoSites2 is a demo upstream site: "http://localhost:5136"
+
+## change list
+
+- 20200914 init projects
---
+++ src/DemoSites/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ProxyKit;
+
+namespace DemoSites
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddProxy();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //proxy sites: http://localhost:5066
+            var upstreamServerHost = "http://localhost:5136";
+            //todo config
+            app.RunProxy(context => context
+                .ForwardTo(upstreamServerHost)
+                .AddXForwardedHeaders()
+                .Send());
+
+            //app.UseWebSockets();
+            //app.UseWebSocketProxy(
+            //    context => new Uri("ws://upstream-host:80/"),
+            //    options => options.AddXForwardedHeaders());
+        }
+    }
+}
